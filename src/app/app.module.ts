d4ce870885a5a380cf1e46972ae46b9656f68812
aymkh <aymen4real@gmail.com>
2022-12-12 08:26:57 +0100

    dummy views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { TypesComponent } from './views/types/types.component';
+import { ManagerComponent } from './views/manager/manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TypesComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
